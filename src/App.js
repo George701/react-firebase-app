44ab55e3090b5101c783896080c49d874a4b77d5
George701 <chernikov.georgii@gmail.com>
2019-03-20 20:41:53 +0100

    Add client, edit client

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import AppNavBar from './COMPONENTS/layout/AppNavBar';
 import Dashboard from './COMPONENTS/layout/Dashboard';
+import AddClient from './COMPONENTS/clients/AddClient';
+import ClientDetails from './COMPONENTS/clients/ClientDetails';
+
 import './App.css';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-        <Router>
-          <div className="App">
-              <AppNavBar/>
-              <div className="container">
-                  <Switch>
-                      <Route exact path="/" component={Dashboard}/>
-                  </Switch>
+        <Provider store={store}>
+            <Router>
+              <div className="App">
+                  <AppNavBar/>
+                  <div className="container">
+                      <Switch>
+                          <Route exact path="/" component={Dashboard}/>
+                          <Route exact path="/client/add" component={AddClient}/>
+                          <Route exact path="/client/:id" component={ClientDetails}/>
+                      </Switch>
+                  </div>
               </div>
-          </div>
-        </Router>
+            </Router>
+        </Provider>
     );
   }
 }
--- src/COMPONENTS/clients/Clients.js
+++ src/COMPONENTS/clients/Clients.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+
+import Spinner from '../layout/Spinner';
 
 class Clients extends Component{
-    render(){
-        const clients = [
-            {
-                id: '434343433',
-                firstName: 'Kevin',
-                lastName: 'Smith',
-                email: 'kevin.smith@gmail.com',
-                phone: '555-555-555',
-                balance: '30'
-            },
-            {
-                id: '37264236',
-                firstName: 'Robert',
-                lastName: 'Paulson',
-                email: 'bob.paul@gmail.com',
-                phone: '444-444-6666',
-                balance: '50'
-            }
-        ];
+    state = {
+        totalOwed: null
+    };
 
+    static getDerivedStateFromProps(props, state){
+        const { clients } = props;
+
+        if(clients){
+            const total = clients.reduce((total, client) => {
+                return total + parseFloat(client.balance.toString())
+            }, 0);
+
+            return { totalOwed: total }
+        }
+        return null;
+    }
+
+    render(){
+        const { clients } = this.props;
+        const { totalOwed } = this.state;
         if(clients){
             return(
                 <div>
@@ -32,7 +38,14 @@ class Clients extends Component{
                                 <i className="fas fa-users"/> Clients{' '}
                             </h2>
                         </div>
-                        <div className="col-md-6"></div>
+                        <div className="col-md-6">
+                            <h5 className="text-right text-secondary">
+                                Total Owed{''}
+                                <span className="text-primary">
+                                    ${parseFloat(totalOwed).toFixed(2)}
+                                </span>
+                            </h5>
+                        </div>
                     </div>
 
                     <table className="table table-striped">
@@ -62,9 +75,19 @@ class Clients extends Component{
                 </div>
             )
         }else{
-            return <h1>Loading...</h1>
+            return <Spinner/>
         }
     }
 }
 
-export default Clients;
+Clients.propTypes = {
+    firestore: PropTypes.object.isRequired,
+    clients: PropTypes.array
+}
+
+export default compose(
+    firestoreConnect([{ collection: 'clients'}]),
+    connect((state, props) => ({
+        clients: state.firestore.ordered.clients
+    }))
+)(Clients);
