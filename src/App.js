26d5088b22eadcb8b34ccfbe7550a49631c3a5f8
George701 <chernikov.georgii@gmail.com>
2019-03-31 22:28:32 +0200

    Redirecting problems

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+ import { UserIsNotAuthenticated, UserIsAuthenticated } from './helpers/auth'
 
 import AppNavBar from './COMPONENTS/layout/AppNavBar';
 import Dashboard from './COMPONENTS/layout/Dashboard';
 import AddClient from './COMPONENTS/clients/AddClient';
 import ClientDetails from './COMPONENTS/clients/ClientDetails';
 import EditClient from './COMPONENTS/clients/EditClient';
+import Login from './COMPONENTS/auth/Login';
 
 import './App.css';
 
@@ -21,10 +23,36 @@ class App extends Component {
                   <AppNavBar/>
                   <div className="container">
                       <Switch>
-                          <Route exact path="/" component={Dashboard}/>
-                          <Route exact path="/client/add" component={AddClient}/>
-                          <Route exact path="/client/edit/:id" component={EditClient}/>
-                          <Route exact path="/client/:id" component={ClientDetails}/>
+                          <Route
+                              exact
+                              path="/"
+                              component={UserIsAuthenticated(Dashboard)}
+                              // component={Dashboard}
+                          />
+                          <Route
+                              exact
+                              path="/client/add"
+                              component={UserIsAuthenticated(AddClient)}
+                              // component={AddClient}
+                          />
+                          <Route
+                              exact
+                             path="/client/edit/:id"
+                             component={UserIsAuthenticated(EditClient)}
+                             // component={EditClient}
+                          />
+                          <Route
+                              exact
+                              path="/client/:id"
+                              component={UserIsAuthenticated(ClientDetails)}
+                              // component={ClientDetails}
+                          />
+                          <Route
+                              exact
+                              path="/login"
+                              component={UserIsNotAuthenticated(Login)}
+                              // component={Login}
+                          />
                       </Switch>
                   </div>
               </div>
--- src/store.js
+++ src/store.js
@@ -25,7 +25,7 @@ const  rrfConfig = {
 
 firebase.initializeApp(firebaseConfig);
 // Init firestore
-const firestore = firebase.firestore();
+// const firestore = firebase.firestore();
 // const settings = {timestampsInSnapshots: true};
 // firestore.settings(settings);
 
