04617ed0b8ec5d34206b542b244cb563a7fbf313
George701 <chernikov.georgii@gmail.com>
2019-05-23 21:15:50 +0300

    Version 0.0.2 Navbar

--- src/App.js
+++ src/App.js
@@ -1,65 +1,23 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
- import { UserIsNotAuthenticated, UserIsAuthenticated } from './helpers/auth'
-
-import AppNavBar from './COMPONENTS/layout/AppNavBar';
-import Dashboard from './COMPONENTS/layout/Dashboard';
-import AddClient from './COMPONENTS/clients/AddClient';
-import ClientDetails from './COMPONENTS/clients/ClientDetails';
-import EditClient from './COMPONENTS/clients/EditClient';
-import Login from './COMPONENTS/auth/Login';
-
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 
-import { Provider } from 'react-redux';
-import store from './store';
+import AppNavbar from './components/layout/AppNavbar';
 
 class App extends Component {
   render() {
     return (
-        <Provider store={store}>
-            <Router>
-              <div className="App">
-                  <AppNavBar/>
-                  <div className="container">
-                      <Switch>
-                          <Route
-                              exact
-                              path="/"
-                              component={UserIsAuthenticated(Dashboard)}
-                              // component={Dashboard}
-                          />
-                          <Route
-                              exact
-                              path="/client/add"
-                              component={UserIsAuthenticated(AddClient)}
-                              // component={AddClient}
-                          />
-                          <Route
-                              exact
-                             path="/client/edit/:id"
-                             component={UserIsAuthenticated(EditClient)}
-                             // component={EditClient}
-                          />
-                          <Route
-                              exact
-                              path="/client/:id"
-                              component={UserIsAuthenticated(ClientDetails)}
-                              // component={ClientDetails}
-                          />
-                          <Route
-                              exact
-                              path="/login"
-                              component={UserIsNotAuthenticated(Login)}
-                              // component={Login}
-                          />
-                      </Switch>
-                  </div>
-              </div>
-            </Router>
-        </Provider>
-    );
+      <Router>
+        <div className="App">
+          <AppNavbar/>
+          <div className="container">
+            <h1>Hello</h1>
+          </div>
+        </div>
+      </Router>
+    )
   }
 }
+
 
 export default App;
--- src/COMPONENTS/layout/AppNavBar.js
+++ src/COMPONENTS/layout/AppNavBar.js
@@ -1,88 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
-import { firebaseConnect } from 'react-redux-firebase';
 
-
-class AppNavBar extends Component {
-    state ={
-        isAuthenticated: false
-    };
-
-    static getDerivedStateFromProps(props, state){
-        const {auth } = props;
-        if(auth.uid){
-            return { isAuthenticated: true}
-        }else{
-            return { isAuthenticated: false}
-        }
-    }
-
-    onLogoutClick = (e) => {
-        e.preventDefault();
-
-        const { firebase } = this.props;
-        firebase.logout();
-    };
-
+class AppNavbar extends Component {
     render() {
-        const { isAuthenticated } = this.state;
-        const { auth } = this.props;
         return (
             <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
                 <div className="container">
                     <Link to="/" className="navbar-brand">
-                        ClientPanel
+                        ClientPannel
                     </Link>
-                    <button
+                    <button 
                         className="navbar-toggler"
                         type="button"
-                        data-toggle="collapse0"
+                        data-toggle="collapse"
                         data-target="#navbarMain"
                     >
-                        <spna className="navber-toggler-icon"></spna>
+                        <span className="navbar-toggler-icon">
+
+                        </span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarMain">
                         <ul className="navbar-nav mr-auto">
-                            {isAuthenticated ? (
-                                <li className="nav-item">
-                                    <Link to="/" className="nav-link">Dashboard</Link>
-                                </li>
-                            ) : null}
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link">
+                                    Dashboard
+                                </Link>
+                            </li>
                         </ul>
-                        {isAuthenticated ? (
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item">
-                                    <a href="#!" className="nav-link">
-                                        { auth.email }
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/" className="nav-link" onClick={this.onLogoutClick}>
-                                        {/*<a className="nav-link" onClick={this.onLogoutClick}>*/}
-                                            Logout
-                                        {/*</a>*/}
-                                    </Link>
-                                </li>
-                            </ul>
-                        ) : null}
                     </div>
                 </div>
             </nav>
-        );
+        )
     }
 }
 
-AppNavBar.propTypes = {
-    firebase: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired
-};
-
-export default compose(
-    firebaseConnect(),
-    connect((state, props) => ({
-        auth: state.firebase.auth
-    }))
-)(AppNavBar);
+export default AppNavbar;
