ee552fd21d0b8ff96a43fa451937f0db2660634e
George701 <chernikov.georgii@gmail.com>
2019-05-24 23:51:44 +0300

    Router and Routes

---
+++ src/COMPONENTS/clients/Clients.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+class Clients extends Component {
+    render() {
+        const clients = [
+            {
+                id: '4343434',
+                firstName: 'Kevin',
+                lastName: "Johnson",
+                email: 'kevin@gmail.com',
+                phone: '333-333-3333',
+                balance: '30'
+            },
+            {
+                id: '44632434',
+                firstName: 'Tomas',
+                lastName: "Edison",
+                email: 'tedison@gmail.com',
+                phone: '333-333-3457',
+                balance: '60'
+            }
+        ];
+
+        if(clients){
+            return (
+                <div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <h2>
+                                <i className="fas fa-users"/> Users
+                            </h2>
+                        </div>
+                        <div className="col-md-6"/>
+                    </div>
+
+                    <table className="table table-striped">
+                        <thead className="thead-inverse">
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Balance</th>
+                                <th/>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {clients.map(client => (
+                                <tr key={client.id}>
+                                    <td>{client.firstName} {client.lastName}</td>
+                                    <td>{client.email}</td> 
+                                    <td>${parseFloat(client.balance).toFixed(2)}</td> 
+                                    <td>
+                                        <Link to={`/client/${client.id}`} className="btn btn-secondary btn-sm">
+                                            <i className="fas fa-arrow-circle-right"/> Details
+                                        </Link>
+                                    </td> 
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )
+        }else{
+            return <div><h1>Loading</h1></div>
+        }
+    }
+}
+
+export default Clients;
