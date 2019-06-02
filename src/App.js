fa773b805e551bfb00f7fd78f4895b3a15e0ac7d
George701 <chernikov.georgii@gmail.com>
2019-06-02 11:49:23 +0300

    Settings Component

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import AddClient from './components/clients/AddClient';
 import ClientDetails from './components/clients/ClientDetails';
 import EditClient from './components/clients/EditClient';
 import Login from './components/auth/Login';
+import Settings from './components/settings/Settings';
 
 class App extends Component {
   render() {
@@ -27,6 +28,7 @@ class App extends Component {
                 <Route exact path="/client/add" component={UserIsAuthenticated(AddClient)} />
                 <Route exact path="/client/:id" component={UserIsAuthenticated(ClientDetails)} />
                 <Route exact path="/client/edit/:id" component={UserIsAuthenticated(EditClient)} />
+                <Route exact path="/settings" component={UserIsAuthenticated(Settings)} />
                 <Route exact path="/login" component={UserIsNotAuthenticated(Login)} />
               </Switch>
             </div>
