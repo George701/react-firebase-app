03806467e4a87eb9d21d02b9f63667e8ace03472
George701 <chernikov.georgii@gmail.com>
2019-06-02 11:01:43 +0300

    Settings reducer & action

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 // Reducers
 import notifyReducer from './reducers/notifyReducer';
+import settignsReducer from './reducers/settignsReducer';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBMcDiBPInpVVutWHesXRnajs3UR4eDqfI",
@@ -39,7 +40,7 @@ const rootReducer = combineReducers({
   firebase: firebaseReducer,
   firestore: firestoreReducer,
   notify: notifyReducer,
-  // settings: settingsReducer
+  settings: settignsReducer
 });
 
 // Check for settings in localStorage
